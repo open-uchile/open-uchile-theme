8acb0adf4e53e8ca467e12ed67f66b443e24698b
Luchin101 <luis.santana101@gmail.com>
2023-10-26 15:31:31 -0300

    redesign explorer course and course list

--- lms/static/js/open_search.js
+++ lms/static/js/open_search.js
@@ -63,45 +63,45 @@ function initDiscovery(search_string=""){
 }
 
 function getData(){
-    var pages = {"page_size": 100, "page_index": index }
+    var pages = {"page_size": 20, "page_index": index }
     var copy = {...filters, ...pages};
     
     $.post( "/search/course_discovery/", copy )
     .done(function( data ) {
-    data.results.forEach(element => {
-        edx.HtmlUtils.append($("#list-courses")[0], createCourse(element.data));
-    });
+        data.results.forEach(element => {
+            edx.HtmlUtils.append($("#list-courses")[0], createCourse(element.data, element.extra_data));
+        });
 
-    currentTotal = currentTotal + data.results.length;
-    if (data.total > currentTotal){
-        index = index + 1;
-        state = 0;
-    }
-    else state = 2;
-    $("#discovery-message").text(T("Viewing")+' ' + data.total + " "+T("courses"));
+        currentTotal = currentTotal + data.results.length;
+        if (data.total > currentTotal){
+            index = index + 1;
+            state = 0;
+        }
+        else state = 2;
+        $(".open-filter-bar #discovery-message").text(T("Viewing")+' ' + data.total + " "+T("courses"));
     });
 }
 
-$('.search-facets-lists button').live('click', function(e) {
+$('.open-filter-bar .search-facets-lists input[type="checkbox"]').live('change', function(e) {
     e.preventDefault();
-    $(this).blur();
+    //$(this).blur();
     let facet = $(this).data("facet");
     let display_name = T($(this).data("text"));
     let add_btn = true;
-    if ($(this).hasClass("selected")){
-        $(this).removeClass("selected");
+    if (this.checked){
+        //$(this).addClass("selected");
+        filters[facet] = $(this).data("value");
+        $('.open-filter-bar .search-facets-lists input[data-facet="'+facet+'"]').not(this).prop( "checked", false );
+    }
+    else{
+        //$(this).removeClass("selected");
         filters[facet] = "";
         add_btn = false;
     }
-    else{
-        $(this).addClass("selected");
-        filters[facet] = $(this).data("value");
-        if($("."+$(this).data("facet")).not(this).hasClass("selected")) $("."+$(this).data("facet")).not(this).removeClass("selected");
-    }
     if (add_btn) AddBtnFilterBar(facet, display_name);
     else {
-        $("#" + facet).remove();
-        if ( $('#active-filters').children().length == 0 ) $('#filter-bar').addClass("is-collapsed");
+        $('.open-filter-bar #active-filters li[data-type="'+facet+'"]').remove();
+        if ( $('.open-filter-bar #active-filters').children().length == 0 ) $('.open-filter-bar #filter-bar').css("display", "none");
     }
 
     let list_course1 = getCourses();
@@ -120,24 +120,65 @@ $('.search-facets-lists button').live('click', function(e) {
     .finally(
         function greet() {
         }
-    );    
+    );
 });
-
-$('#clear-all-filters').live('click', function(e) {
+$('#FilterOffcanvasBottom .search-facets-lists input[type="checkbox"]').live('change', function(e) {
     e.preventDefault();
-    $('#filter-bar').addClass("is-collapsed");
-    $("#active-filters").html('');
-    $('.search-facets-lists button.selected').removeClass("selected");
+    $('.open-filter-bar .search-facets-lists input[data-facet="'+$(this).data("facet")+'"][data-value="'+$(this).data("value")+'"]').trigger( "click" );
+    $('#FilterOffcanvasBottom button.btn-close').trigger( "click" );
+});
+$('.open-filter-bar #clear-filters').live('click', function(e) {
+    e.preventDefault();
+    $('.open-filter-bar #filter-bar').css("display", "none");
+    $(".open-filter-bar #active-filters").html('');
+    $('.open-filter-bar .search-facets-lists input').prop( "checked", false );
     cleanCourses();
     initDiscovery();
 });
-$('#filter-bar .facet-option.discovery-button').live('click', function(e) {
+$('#FilterOffcanvasBottom #clear-filters').live('click', function(e) {
     e.preventDefault();
-    let btnType = $(this).data('type');
-    $("#" + btnType).remove();
-    if ( $('#active-filters').children().length == 0 ) $('#filter-bar').addClass("is-collapsed");
+    $('.open-filter-bar a#clear-filters').trigger( "click" );
+    $('#FilterOffcanvasBottom button.btn-close').trigger( "click" );
+});
+$('.open-order-by-btn').live('click', function(e) {
+    e.preventDefault();
+    let facet = 'order_by';
+    let value = $(this).data("value");
+    if($(this).hasClass( "open-order-by-selected" )){
+        filters[facet] = "";
+        $(this).removeClass("open-order-by-selected");
+    }
+    else{
+        $('.open-order-by-btn').not(this).removeClass("open-order-by-selected");
+        $(this).addClass( "open-order-by-selected" );
+        filters[facet] = value;
+    }
+    
+    let list_course4 = getCourses();
+    // executes when promise is resolved successfully
+    list_course4.then(
+        function successValue(result) {
+            //console.log(result);
+        },
+    )
+    // executes if there is an error
+    .catch(
+        function errorValue(result) {
+            console.log(result);
+        }
+    )
+    .finally(
+        function greet() {
+        }
+    );
+});
+$('.open-filter-bar #filter-bar #active-filters span.fa-times').live('click', function(e) {
+    e.preventDefault();
+    let btnType = this.parentElement.dataset.type;
+    $(this.parentElement).remove();
+    if ( $('.open-filter-bar #active-filters').children().length == 0 ) $('.open-filter-bar #filter-bar').css("display", "none");
     filters[btnType] = "";
-    $('.search-facets-lists button.'+btnType+'.selected').removeClass("selected");
+    $('.open-filter-bar .search-facets-lists input[data-facet="'+btnType+'"]').not(this).prop( "checked", false );
 
     let list_course2 = getCourses();
     // executes when promise is resolved successfully
@@ -159,11 +200,16 @@ $('#filter-bar .facet-option.discovery-button').live('click', function(e) {
         }
     );
 });
-
+$('#FilterOffcanvasBottom #filter-bar #active-filters span.fa-times').live('click', function(e) {
+    e.preventDefault();
+    var $parent = $(this).parent();
+    $('.open-filter-bar #filter-bar #active-filters li[data-type="'+$parent.data("type")+'"][data-value="'+$parent.data("value")+'"] span.fa-times').trigger( "click" );
+    $('#FilterOffcanvasBottom button.btn-close').trigger( "click" );
+});
 $('#discovery-submit').live('click', function(e) {
     e.preventDefault();
-    $('.icon.fa.fa-search').css("visibility", "hidden");
-    $('#loading-indicator').toggleClass('hidden');
+    //$('.icon.fa.fa-search').css("visibility", "hidden");
+    //$('#loading-indicator').toggleClass('hidden');
     filters["search_string"] = $("#discovery-input").val();
     let list_course3 = getCourses();
     // executes when promise is resolved successfully
@@ -180,17 +226,51 @@ $('#discovery-submit').live('click', function(e) {
         }
     )
     .finally(
-    function greet() {
-        $('.icon.fa.fa-search').css("visibility", "visible");
-        $('#loading-indicator').toggleClass('hidden');
-    }
+        function greet() {
+            //$('.icon.fa.fa-search').css("visibility", "visible");
+            //$('#loading-indicator').toggleClass('hidden');
+        }
     );
 });
-
+$('#discovery-input').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+        filters["search_string"] = $("#discovery-input").val();
+        let list_course3 = getCourses();
+        // executes when promise is resolved successfully
+        list_course3.then(
+            function successValue(result) {
+                //console.log(result);
+                if (filters["search_string"] != "") AddBtnFilterBar("search_string", filters["search_string"]);
+            },
+        )
+        // executes if there is an error
+        .catch(
+            function errorValue(result) {
+                console.log(result);
+            }
+        )
+        .finally(
+            function greet() {
+                //$('.icon.fa.fa-search').css("visibility", "visible");
+                //$('#loading-indicator').toggleClass('hidden');
+            }
+    );
+    }
+});
+$('#open-filter-mobile-btn').live('click', function(e) {
+    e.preventDefault();
+    $('#open-filter-mobile-body').empty()
+    $('#open-filter-mobile-body').html($('.open-filter-bar').html());
+    
+    $('.open-filter-bar .search-facets-lists input[type="checkbox"]').each(function( ) {
+        if (this.checked) $('#FilterOffcanvasBottom .search-facets-lists input[data-facet="'+$(this).data("facet")+'"][data-value="'+$(this).data("value")+'"]').prop( "checked", true );
+    });
+});
 function T(w){
     // translate to spanish
     if(document.documentElement.lang == "es-419" && translation[w] !== undefined ){
-    return translation[w];
+        return translation[w];
     }
     else return w;
 }
@@ -199,15 +279,15 @@ function getCourses(){
     cleanCourses();
     // returns a promise
     return new Promise(function (resolve, reject) {
-    getData();
-    //pasar datos a resolve
-    resolve('Promise success');
-    reject('Promise rejected');
+        getData();
+        //pasar datos a resolve
+        resolve('Promise success');
+        reject('Promise rejected');
     });
     
 }
 
-function createCourse(data){
+function createCourse(data, extra_data){
     /*
     "data": {
         "id": "course-v1:eol+NA303+2022",
@@ -227,40 +307,71 @@ function createCourse(data){
         "modes": [
             "audit"
         ],
-        "language": "en"
+        "language": "en",
+        'short_description', 'advertised_start', 'display_org_with_default', 'main_classification'
         }
     */
-    //var org = "";
-    //if (fake_orgs[data.id] !== undefined) org = "open_org_"+fake_orgs[data.id];
-    const coursehtml = '<li class="courses-listing-item"><article class="course" role="region" aria-label="{course_display_name}">'+
-    '<a href="/courses/{course}/about"><header class="course-image">'+
-    '<div class="cover-image"><img src="{image_url}" alt="{course_display_name}">'+
-    '<div class="learn-more" aria-hidden="true">APRENDER MAS</div></div></header>'+
-    '<section class="course-info" aria-hidden="true"><h2 class="course-name">'+
-    '<span class="course-organization">{org}</span><span class="course-code">{number}</span><span class="course-title">{course_display_name}</span>'+
-    '</h2><div class="course-date" aria-hidden="true">{formatDate}</div>'+
-    '</section><div class="sr"><ul><li>{org}</li><li>{number}</li><li><time itemprop="startDate" datetime="{start}">{formatDate}</time></li>'+
-    '</ul></div></a></article></li>';
+    let img_html = '';
+    let org_html = '<p class="card-text ct1" title="{org}"><small>{org}</small></p>'
+    if ( extra_data.main_classification.logo ) {
+        img_html = '<img class="card-logo" src="{mainClass_logo}">';
+        data["mainClass_logo"] = extra_data.main_classification.logo;
+        org_html = '';
+    }
+    const coursehtml = '<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-3"><div class="card mb-3 ${card_class}" data-about="/courses/{course}/about" style="cursor: pointer;" onclick="window.location.href = this.dataset.about"><div class="row g-0"><div class="col-md-7"><figure>'+
+    '<img src="{image_url}" class="card-img-top img-fluid rounded-start" alt="{course_display_name}"></figure></div>'+
+    '<div class="col-md-5"><div class="card-body">'+img_html+'<h5 class="card-title" title="{course_display_name}">{course_display_name}</h5>'+
+    org_html+'<p class="card-text ct2" title="{course_overview}"><small>{course_overview}</small></p>'+
+    '<div class="row ct3">{course_date_html}</div><div class="card-button"><a href="/courses/{course}/about"><button type="button" class="btn btn-outline-light">APRENDER MAS</button></a></div>'+
+    '</div></div></div></div></div>';
+    data['course_date_html'] = create_course_date_html(data.start, data.end, extra_data.advertised_start)
     data["course_display_name"] = data.content.display_name;
-    data["formatDate"] = formatDate(data.start, data.end);
+    data["course_overview"] = extra_data.short_description || data.content.overview;
+    if(extra_data.display_org_with_default != data["org"]) data["org"] = extra_data.display_org_with_default;
+    else data["org"] = extra_data.main_classification.name || data.org;
+    //data["formatDate"] = formatDate(data.start, data.end);
     return edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(coursehtml), data);
+}
+
+function create_course_date_html(start, end, advertised_start){
+    const html1 = '<div class="col-md-12"><div class="open-course-date-icon"><img src="/static/open-uchile-theme/images/svg-2023/fecha termino.svg"></div>'+
+    '<div class="open-course-date-text"><span>{date_text}</span></div></div>';
+    const html2 = '<div class="col-md-6"><div class="open-course-date-icon"><img src="/static/open-uchile-theme/images/svg-2023/fecha inicio.svg"></div>'+
+    '<div class="open-course-date-text"><span>Inicio del curso</span>'+
+    '<div class="course-date" aria-hidden="true">{start_date}</div> </div>'+
+    '</div><div class="col-md-6"><div class="open-course-date-icon"><img src="/static/open-uchile-theme/images/svg-2023/fecha termino.svg"></div>'+
+    '<div class="open-course-date-text"><span>Termino del curso</span>'+
+    '<div class="course-date" aria-hidden="true">{end_date}</div></div></div>';
+    var start_date  = new Date(start);
+    var date_data = {
+        'start_date': translate_date(start_date),
+        'end_date': ''
+    };
+    if (end !== undefined){
+        var end_date = new Date(end);
+        date_data['end_date'] = translate_date(end_date);
+        if(end_date < now) return edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(html1), {'date_text': T("Finished course")});
+    }
+    if(advertised_start !== undefined && advertised_start != null && advertised_start != '') return edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(html1), {'date_text': advertised_start});
+    if(now >= start_date) return edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(html1), {'date_text': T("Permanently open")});
+    else return edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(html2), date_data);
 }
 
 function formatDate(start, end){
     var start_date  = new Date(start);
     if (end !== undefined){
-    var end_date = new Date(end);
-    if(end_date < now) return T("Finished course");
-    else return translate_date(start_date);
+        var end_date = new Date(end);
+        if(end_date < now) return T("Finished course");
+        else return translate_date(start_date);
     }
     else{
-    if(now >= start_date) return T("Permanently open");
-    else return translate_date(start_date);
+        if(now >= start_date) return T("Permanently open");
+        else return translate_date(start_date);
     }
 }
 
 function translate_date(date){
-    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var options = { year: 'numeric', month: 'short', day: 'numeric' };
     if(document.documentElement.lang == "es-419" ){
     return date.toLocaleDateString("es-ES", options);
     }
@@ -268,7 +379,7 @@ function translate_date(date){
 }
 
 function AddBtnFilterBar(type, value){
-    if ( $('#active-filters').children().length == 0 ) $('#filter-bar').removeClass("is-collapsed");
+    if ( $('#active-filters').children().length == 0 ) $('.open-filter-bar #filter-bar').css("display", "block");
     createBtnActiveFilters(type, value)
 }
 
@@ -279,11 +390,10 @@ function AddBtnFilterBar(type, value){
 }*/
 
 function createBtnActiveFilters(type, value){
-    $("#" + type).remove();
-    let buttons = '<li class="active-filter" id="{type}"><button data-value="{value}" class="facet-option discovery-button" data-type="{type}">'+
-        '<span class="query">"{value}"</span><span aria-hidden="true" class="fa fa-times"></span></button></li>';
+    $('#active-filters li[data-type="'+type+'"]').remove();
+    let buttons = '<li data-type="{type}" data-value="{value}">{value}<span aria-hidden="true" class="fa fa-times"></span></li>'
     let filterBtn = edx.HtmlUtils.interpolateHtml(edx.HtmlUtils.HTML(buttons), {type:type, value:value});
-    edx.HtmlUtils.append($("#active-filters")[0], filterBtn);
+    edx.HtmlUtils.append($(".open-filter-bar #active-filters")[0], filterBtn);
 }
 function removeBtnActiveFilters(type, value){
     $("#" + type).remove();
@@ -293,7 +403,7 @@ function cleanCourses(){
     index = 0;
     currentTotal = 0;
     state = 1;
-    $("#discovery-message").text("");
+    $(".open-filter-bar #discovery-message").text("");
     $("#list-courses").empty();
 }
 })
